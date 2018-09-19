eda3b661b3e5fb2618aa18f1e306067a67b3e078
arrobin <arrobin02@gmail.com>
2018-09-19 22:12:53 +0600

    Add project files.

---
+++ SimpleEmployeeInformationSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleEmployeeInformationSystem.Startup))]
+namespace SimpleEmployeeInformationSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
